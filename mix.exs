a565f2e87107f6cb9c813e043726cd1947493680
Jesper Christiansen <hi@jespr.com>
2017-05-07 13:00:06 -0700

    Added comeonin, ecto and postgres

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Noegle.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger, :comeonin]]
+    [extra_applications: [:logger, :comeonin, :ecto, :postgrex]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,10 @@ defmodule Noegle.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:comeonin, "~> 2.3"}]
+    [
+      {:comeonin, "~> 2.3"},
+      {:ecto, "~> 2.0.0-rc"},
+      {:postgrex, ">= 0.0.0", only: :test},
+    ]
   end
 end
